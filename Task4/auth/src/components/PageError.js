12e6d37fd134bed54e6e93ffa5fadd2f6e23b8da
SoumyaMoturi <69074611+SoumyaMoturi@users.noreply.github.com>
2022-06-02 12:38:16 +0530

    Update PageError.js

--- Task4/auth/src/components/PageError.js
+++ Task4/auth/src/components/PageError.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PageError = () => {
   return (
-    <div styles={{"backgroundColor":"black","color":"white"}}>
+    <div style={{"backgroundColor": "#3e5759","color":'white',"top":"25%","left":"50%","transform":"translate(-50%,-50%)","padding":"100px","position":"fixed","border":"5px double white","borderRadius":"15px",}}>
       <h1>Error 404:Page not found</h1>
     </div>
   )
